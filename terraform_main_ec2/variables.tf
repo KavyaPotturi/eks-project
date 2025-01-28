ddf193806ead02f857833bd10f2780ce975bf2cc
KavyaPotturi <150267961+KavyaPotturi@users.noreply.github.com>
2025-01-28 21:15:11 +0530

    Update variables.tf

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -49,7 +49,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-01816d07b1128cd2d" // Replace with the latest AMI ID for your region
+  default     = "ami-0ac4dfaf1c5c0cce9" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
